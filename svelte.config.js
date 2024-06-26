051f9f42972df67d804a68df9892afcece0bfc16
mimhle <40392166+mimhle@users.noreply.github.com>
2024-06-26 16:07:19 +0700

    Refactor

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ export default {
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
-			assets: 'build/assets',
+			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true
